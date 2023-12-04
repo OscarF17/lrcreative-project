909631788c26314024691eaf61455e16f325be10
Oscar Flores <oscar.floresl@udem.edu>
2023-12-03 21:07:08 -0600

    updated login

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ app.config['MYSQL_USER'] = 'web_user'
 app.config['MYSQL_PASSWORD'] = 'password'
 app.config['MYSQL_DB'] = 'LRCreative'
 
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+
 @app.route('/')
 def index():
   return render_template('index.html')
@@ -79,32 +81,51 @@ def conocenos():
 def faq():
   return render_template('faq.html')
 
-@app.route('/loginAdmin', methods=['POST'])
+
+@app.route('/loginAdmin')
 def loginAdmin():
-    
-    # Conseguir todo del forms
-    nombreAdmin = request.form['nombreAdmin']
-    password = request.form['password']
+  return render_template('login.html')
 
-    # Conecta con la base de datos
-    cursor = mysql.connection.cursor()
+@app.route('/admin', methods=['GET', 'POST'])
+def admin():
 
-    # Busca al usuario en la base de datos
-    cursor.execute("SELECT id, nombre, password, mail FROM Usuario WHERE nombre = %s", (nombreAdmin,))
-    usuario = cursor.fetchone()
-
-    # Cerrar Cursor
-    cursor.close()
-
-    # Verifica la existencia del usuario y verificar contraseña
-    if usuario and check_password_hash(usuario[2], password): # CHECAR HASH
-        
-        # Establece la información del usuario en la sesión
-        session['user_info'] = {'id': usuario[0], 'nombre': usuario[1], 'correo': usuario[3]}
-        
-        return render_template('admin.html')
+    if session:
+      print("sesion no vacia")
     else:
-        return redirect(url_for('/login'))
+      print("sesion vacia")
+    
+    if request.method == 'POST':
+      # Conseguir todo del forms
+      nombreAdmin = request.form['nombreAdmin']
+      password = request.form['password']
+
+      # Conecta con la base de datos
+      cursor = mysql.connection.cursor()
+
+      # Busca al usuario en la base de datos
+      cursor.execute("SELECT id, nombre, password, email FROM Usuario WHERE email = %s", (nombreAdmin,))
+      usuario = cursor.fetchone()
+
+      # Cerrar Cursor
+      cursor.close()
+
+      # Verifica la existencia del usuario y verificar contraseña
+      if usuario and check_password_hash(usuario[2], password): # CHECAR HASH
+          
+          # Establece la información del usuario en la sesión
+          session['user_info'] = {'id': usuario[0], 'nombre': usuario[1], 'correo': usuario[3]}
+          
+          return render_template('admin.html')
+      else:
+          return redirect(url_for('loginAdmin'))
+    elif request.method == 'GET':
+      print(session)
+      if session:
+        return render_template('admin.html')
+      else:
+        return redirect('/loginAdmin')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
